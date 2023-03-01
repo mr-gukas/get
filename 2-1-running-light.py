51ac6fea0e9974037e5d7fb61a62348d6b89c0d9
mr-gukas <frodo615@yandex.ru>
2023-03-01 13:49:11 +0300

    first projects

---
+++ 2-1-running-light.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+
+leds = [21, 20, 16, 12, 7, 8, 25, 24]
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup  (leds, GPIO.OUT)
+
+for i in range(24):
+    GPIO.output(leds[i % 8], 1)
+    time.sleep(0.2)
+    GPIO.output(leds[i % 8], 0)
+
+
+GPIO.output(leds, 0)
+GPIO.cleanup()
